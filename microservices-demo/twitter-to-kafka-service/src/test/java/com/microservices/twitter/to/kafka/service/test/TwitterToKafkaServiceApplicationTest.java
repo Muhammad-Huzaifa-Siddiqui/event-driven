9dd2f3f4b5c318cd0d5fa718da67dd8cdba17232
Muhammad-Huzaifa-Siddiqui <40765078+Muhammad-Huzaifa-Siddiqui@users.noreply.github.com>
2022-01-13 03:35:35 +0500

    Add files via upload

---
+++ microservices-demo/twitter-to-kafka-service/src/test/java/com/microservices/twitter/to/kafka/service/test/TwitterToKafkaServiceApplicationTest.java
@@ -0,0 +1,12 @@
+package com.microservices.twitter.to.kafka.service.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class TwitterToKafkaServiceApplicationTest {
+    @Test
+    public void contextLoad(){
+
+    }
+}
